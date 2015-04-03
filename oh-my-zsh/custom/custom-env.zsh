132e76f30f76386e6804d79e1f5b5abbc545d45e
Daisuke Miyamoto <dai.0304@gmail.com>
2015-04-03 16:36:28 +0900

    Add docker configuration

--- oh-my-zsh/custom/custom-env.zsh
+++ oh-my-zsh/custom/custom-env.zsh
@@ -35,7 +35,9 @@ export GRADLE_OPTS="-Dorg.gradle.daemon=true -Xmx1024m -Xms256m -XX:+CMSClassUnl
 export MAVEN_OPTS="-Xmx1024M"
 
 # ==== docker
-export DOCKER_HOST=tcp://localhost:4243
+export DOCKER_HOST=tcp://192.168.59.103:2376
+export DOCKER_CERT_PATH=$HOME/.boot2docker/certs/boot2docker-vm
+export DOCKER_TLS_VERIFY=1
 
 # ==== other
 bindkey '^R' history-incremental-pattern-search-backward
